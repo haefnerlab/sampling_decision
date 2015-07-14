1304dd6c24779b5198f47ad83c835f0a59f04ab5
ralf <ralf.haefner@gmail.com>
2015-07-14 16:44:10 -0400

    Change paras for corr-performance figure

--- S_Exp_Para.m
+++ S_Exp_Para.m
@@ -84,14 +84,14 @@ switch mode
     P.number_orientations=2;
     P.prior_task=[1 0]; % [cardinal, oblique]
     P.number_locations=1;
-    P.dimension_X=512;
+    P.dimension_X=256;
     P.dimension_G=64;
     P.kappa_O=[1 0]; % attended and unattended
     P.kappa_G=3;
     P.delta=0.08; % strength of X-G coupling for corr & CPs, 80/1024
     P.stimulus_regime='static';
     P.stimulus_contrast=zeros(1,P.number_orientations);
-    P.number_repetitions=512; % number of trials
+    P.number_repetitions=1024; % number of trials
     P.n_zero_signal=20; % number of frames before onset of stimulus
     P.number_burn_in=0; % number of burn-in samPles
     P.number_samples_to_use=100; %Number  of non-burn samples to be used for evidence
