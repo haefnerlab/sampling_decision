d8b618246272d4c5fd10054198e41f433259cf89
wrongu <rlange@ur.rochester.edu>
2015-08-21 09:38:42 -0400

    BackwardsComp moved to inside S_Experiment (only used there)

--- S_Experiment.m
+++ S_Experiment.m
@@ -156,3 +156,43 @@ else task='discrimination'; end
     out = BackwardsComp(out);
 
 end
+
+function out = BackwardsComp(out)
+%Manually handle variables whose names have changed
+out.Projection.nO = out.Projection.G.number_orientations;
+out.Projection.nL = out.Projection.G.number_locations;
+out.Projection.nX = out.Projection.G.dimension_X;
+out.Projection.nG = out.Projection.G.dimension_G;
+out.Projection.pT = out.Projection.G.prior_task;
+out.Projection.phi_x = out.Projection.G.phi_x;
+out.Projection.phi_g = out.Projection.G.phi_g;
+out.InputImage.dyn = out.Projection.I.stimulus_regime;
+out.InputImage.c = out.Projection.I.stimulus_contrast;
+out.Sampling.n_burn = out.Projection.S.number_burn_in;
+out.Sampling.n_use = out.Projection.S.number_samples_to_use;
+out.Sampling.nsamples_per_evidence = out.Projection.G.number_samples_per_evidence;
+out.Projection.delta = out.Projection.G.delta;
+out.Projection.kappa_O = out.Projection.G.kappa_O;
+out.Projection.kappa_G = out.Projection.G.kappa_G;
+
+
+%Automatically update non-name changing variables
+names = fieldnames(out.Projection.G);
+data = struct2cell(out.Projection.G);
+l = length(names);
+for i=1:l;
+    out.Projection.(names{i}) = data{i};
+end
+names = fieldnames(out.Projection.S);
+data = struct2cell(out.Projection.S);
+l = length(names);
+for i=1:l;
+    out.Projection.(names{i}) = data{i};
+end
+names = fieldnames(out.Projection.I);
+data = struct2cell(out.Projection.I);
+l = length(names);
+for i=1:l;
+    out.Projection.(names{i}) = data{i};
+end
+end
