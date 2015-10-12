b8190f4a32e592298c2fd98a034deccef9eb030f
wrongu <rlange@ur.rochester.edu>
2015-10-12 14:18:21 -0400

    added Orientation_Tuning_Like(e) to get all orient-tuning for models e (a small but useful abstraction)

---
+++ Orientation_Tuning_Like.m
@@ -0,0 +1,14 @@
+function [ models_tuning ] = Orientation_Tuning_Like( e, n_orient, trials )
+%ORIENTATION_TUNING_LIKE given results of running experiments 'e', which is
+%a cell array of results for various model and image paramters, this
+%function makes a cell array of the same size as 'e' that contains
+%orientation tuning results for each of the corresponding models.
+
+models_tuning = cell(size(e));
+
+for i=1:numel(e)
+    model = e{i};
+    models_tuning{i} = Orientation_Tuning_Response(model.Projection, model.Sampling, n_orient, trials);
+end
+
+end
