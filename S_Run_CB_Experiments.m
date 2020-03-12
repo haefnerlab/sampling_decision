84d4353702bc83775a2665e3ae343c4fea54e5ec
wrongu <rlange@ur.rochester.edu>
2020-03-12 15:35:00 -0400

    Trying shuffled (rather than switching) frames in S_Run_CB_Experiments, and set HSLC parameters for kappa

--- S_Run_CB_Experiments.m
+++ S_Run_CB_Experiments.m
@@ -4,31 +4,37 @@ function [lshc_results, hslc_results] = S_Run_CB_Experiments(varargin)
 % create simulations for coupling strength dependency figure
 para = S_Exp_Para('cb');
 
+% Uncomment one of the two following
+stimtype = '_shuffled';
+para.I.stimulus_regime = 'dynamic-switching-signal-blocked';
+% stimtype = '';
+% para.I.stimulus_regime = 'dynamic-switching-signal-blocked';
+
 savedir = pwd;
 if isfield(para, 'savedir'), savedir = para.savedir; end
 
 %% RUN LSHC
 para.S.number_repetitions = 1000;
-para.G.delta = .08;
+para.G.kappa_O = [1 0]; % attended and unattended
 para.I.stimulus_contrast = [+4 +4];
-para.I.signal_match_probability = .9;
-lshc_results = runWithParams(para, savedir);
+para.I.signal_match_probability = 10/11;
+lshc_results = runWithParams(para, savedir, stimtype);
 CB_Diagnostics(lshc_results, 'LSHC Condition');
 
 %% RUN HSLC
 para.S.number_repetitions = 1000;
-para.G.delta = .01;
-para.I.stimulus_contrast = [+18 +18];
-para.I.signal_match_probability = .6;
-hslc_results = runWithParams(para, savedir);
+para.G.kappa_O = [.1 0]; % attended and unattended
+para.I.stimulus_contrast = [+8 +8];
+para.I.signal_match_probability = 6/11;
+hslc_results = runWithParams(para, savedir, stimtype);
 CB_Diagnostics(hslc_results, 'HSLC Condition');
 end
 
-function results = runWithParams(para, savedir)
+function results = runWithParams(para, savedir, stimtype)
 p_match = max(para.I.signal_match_probability, 1-para.I.signal_match_probability);
 
-savename = sprintf('sampling_results_tr%d_c%.2f_pm%.2f_d%.3f.mat', ...
-    para.S.number_repetitions, para.I.stimulus_contrast(1), p_match, para.G.delta);
+savename = sprintf('sampling_results%s_tr%d_c%.2f_pm%.2f_k%s.mat', ...
+    stimtype, para.S.number_repetitions, para.I.stimulus_contrast(1), p_match, num2str(para.G.kappa_O));
 savefile = fullfile(savedir, savename);
 
 if exist(savefile, 'file')
